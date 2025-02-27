464ae3cff539eeb2c6923e7756c683b32caa54b1
RageLessLion <alexandru.carutasu34@gmail.com>
2025-02-27 21:14:48 +0200

    added ORM data

---
+++ database/migrations/2025_02_27_190329_car.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('car', function (Blueprint $table) {
+            $table->id();
+            $table->string('model');
+            $table->string('resgistration_number');
+            $table->timestamp('model_year')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
